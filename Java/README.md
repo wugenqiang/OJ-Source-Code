9b6d17f2634cbe804629683b4c1cf47f3e2d8b60
WuGenQiang <2422676183@qq.com>
2020-04-21 11:53:17 +0800

    auto update upload

--- Java/README.md
+++
@@ -1,2 +0,0 @@
-# SwordForOffer
-剑指Offer C++ 和 Java 描述
--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ git add .
 
 git status
 
-git commit -m 'auto update https://github.com/wugenqiang/SwordForOffer.git'
+git commit -m 'auto update upload'
 
 echo '--------commit successfully--------'
 
-git push -u https://github.com/wugenqiang/SwordForOffer.git master
+git push -u https://github.com/wugenqiang/OJ-Source-Code.git master
 # git push -u origin master
 
 echo '--------push to GitHub successfully--------'
